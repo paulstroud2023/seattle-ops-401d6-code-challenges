52ede0d114859b7954a88e8f17c4411e1149d406
Paul Stroud <paul.stroud2023@gmail.com>
2023-04-19 03:40:03 +0000

    50% functional

--- 02. uptime monitor v1.py
+++ 02. uptime monitor v1.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 # Script: Ops 401 Class 02 script
 # Author: Paul Stroud
 # Date of latest revision: 04/18/23
@@ -6,7 +9,41 @@
 
 # Main
 
-import os, datetime, time
+import sys, os, datetime, time, re, subprocess
+
+ip_regex = "^(\d{1,3}\.){3}\d{1,3}$"  # define the regex
+# could also use ^([0-9]{1,3}\.){3}[0-9]{1,3}$
+# both regex checks for a string that contains 3x groups of 1-3 numbers
+# where each decimal digit is between 0-9
+# the groups are separated by a '.'
+# the string ends with another group of 1-3 digits between [0-9]
+
+def IP_validation(addr):
+    if re.match(ip_regex, addr):
+       return True
+    else: return False
+
+
+#print(f"The script has {len(sys.argv) - 1} args\narg[0]={sys.argv[0]}")
+
+if len(sys.argv) == 2:  # is there a script arg?
+  ip_addr = sys.argv[1]
+#   ip_regex = "^(\d{1,3}\.){3}\d{1,3}$"  # define the regex 
+#   print(dir(re.match(ip_regex, sys.argv[1])))
+  if IP_validation(ip_addr):    # verify the IP is valid, otherwise abort
+    # do useful stuff
+    print(f"[ Monitoring uptime for: {ip_addr} ]")
+    ping_result = subprocess.run(["ping", "-c 1", ip_addr], capture_output=True, text=True)
+    print(datetime.datetime.now(), "\t", ping_result.returncode == 0, "\n", ping_result.stdout)
+  else: print("Invalid IP detected. Please try again.")
+
+
+# if len(sys.argv) == 1:
+#     # The script has command-line arguments
+#     print("YES")
+# else:
+#     # The script has no command-line arguments
+#     print("NO")
 
 # - Transmit a single ICMP (ping) packet to a specific IP every two seconds.
 # - Evaluate the response as either success or failure.
@@ -18,7 +55,11 @@ import os, datetime, time
 # - Save the output to a text file as a log of events.
 # - Accept user input for target IP address.
 
-print("badabing badaboom")
+
+
+
+
+# print("badabing badaboom")
 
 
 
