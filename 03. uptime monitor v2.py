454a1c1317e9e0b04ad9d5083a86c52caec6ca32
Paul Stroud <paul.stroud2023@gmail.com>
2023-04-20 05:03:52 +0000

    done

--- 03. uptime monitor v2.py
+++ 03. uptime monitor v2.py
@@ -26,7 +26,9 @@ import re           # for regex
 import subprocess   # for running bash commands
 
 import smtplib
-import ssl
+# import ssl
+from email.message import EmailMessage  # to craft emails
+import os
 
 
 ###### GLOBAL VARS ########
@@ -40,6 +42,7 @@ ip_regex = "^(\d{1,3}\.){3}\d{1,3}$"
 # the string ends with another group of 1-3 digits between [0-9]
 
 ip_status = { 0:"UP", 1:"DOWN" }
+flair = [ "✓", "✗" ]
 
 
 ###### FUNCTIONS ########
@@ -54,9 +57,27 @@ def print_log(str, file):  # str needs to end with '\n'
     file.write(str)     # entry in the log file
     print(str, end="")  # message to console
    
-# def node_alert(ip, status):
-   # status is boolean/binary
-   # 1 == up, 0 == down
+
+# def send_email(msg, addr):
+#    print(f'Sending {msg} to {addr}')
+
+def send_email(subj, msg, rcvr, sdr, sdr_pw, smtp_svr="smtp.gmail.com", smtp_port=587):
+    #print(f'Sending {msg} to {rcvr}')
+    SMTP = smtplib.SMTP(smtp_svr, smtp_port)
+    SMTP.starttls()
+    SMTP.login(sdr, sdr_pw)
+
+    em = EmailMessage()
+    em['From'] = sdr
+    em['To'] = rcvr
+    em['Subject'] = subj
+    em.set_content(msg)
+
+    test = SMTP.sendmail(sdr, rcvr, em.as_string())
+    #print("Message status:", test)
+
+    SMTP.quit()
+
 
 ###### MAIN CODE BELOW ########
 
@@ -78,10 +99,34 @@ if (not IP_validation(ip_addr)):    # check if IP is invalid
 
 
 
-# all checks complete
+# attempt to read email info from a text file
+# the format for the text file is:
+# <sender addr>
+# <SMTP password (sender)>
+# <receiver addr>
+# there is no input validation
+# no leading spaces or any other formatting
+if (os.path.exists("email.txt")): # if file exists, read credentials 
+  print("Loading email info for alerts... ", end="")
+  FILE = open("email.txt", "r")  # open the file
+  # read each line into its var, strip the trailing newline
+  sender = FILE.readline().replace("\n", "")
+  sender_pw = FILE.readline().replace("\n", "")
+  receiver = FILE.readline().replace("\n", "")
+  FILE.close()    # close the file stream
+  print('OK')
+else:   # manually enter the info
+  print("Enter the following information for email alerts:")
+  sender = input("\tSender email: ")
+  sender_pw = input("\tSMTP password: ")
+  receiver = input("\tRecipient email: ")
+
+
+# we're ready to go
 # now let's do useful stuff
 
-print(f"[ Monitoring uptime for: {ip_addr} ]")
+
+print(f"\n[ Monitoring uptime for: {ip_addr} ]")
 
 ping_count = 0  # all pings
 ping_ok = 0     # good pings
@@ -92,21 +137,28 @@ try:  # you can't fail if you don't try
     while True:     # to infinity and beyond!
         # run a ping cmd and save results to a var
         ping_result = subprocess.run(["ping", "-c 1", ip_addr], capture_output=True, text=True)
-        
-        ping_ok += 0 if ping_result.returncode else 1   # ++ only good pings
+        pstat = ping_result.returncode # shortcut var for status code (0 is good, 1 is bad)
+
+        # pull current date/time, format, and save to a var
+        timecode = datetime.datetime.now().strftime("%Y%m%d %H:%M:%S")
+
+        ping_ok += 0 if pstat else 1   # ++ only good pings
         ping_count += 1  # ++ all attempts
 
         # craft a formatted log entry that includes date/time and a status message
-        log_str = datetime.datetime.now().strftime("%Y%m%d %H:%M:%S") + " > " + \
-                  f"Host {ip_addr} is " + ("DOWN" if ping_result.returncode else "UP") + "\n"
+        log_str = timecode + " > " + f"Host {ip_addr} is " + ip_status[pstat] + " " + flair[pstat] + "\n"
         
         print_log(log_str, FILE)
         print("Ctrl+C to stop", end="\r")   # floating message
 
-        if (last_ping != ping_result.returncode):
-          print(f"ALERT: Host ({ip_addr}) status changed from {ip_status[last_ping]} to {ip_status[ping_result.returncode]}")
+        if (last_ping != pstat): # status change?
+          alert_str = f"Host ({ip_addr}) status changed from {ip_status[last_ping]} to {ip_status[pstat]}"
+          print_log(timecode +  " > ALERT: " + alert_str + "\n", FILE)
+          # alert the admin
+          send_email(f"UPTIME ALERT: Host {ip_addr} is {ip_status[pstat]}", 
+                     timecode + " > " + alert_str, receiver, sender, sender_pw)
 
-        last_ping = ping_result.returncode
+        last_ping = pstat
         time.sleep(2)   # 2 sec timeout
 
 except KeyboardInterrupt:   # Ctrl+C
