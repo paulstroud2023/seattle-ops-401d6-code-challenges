993873708cd034b88cefa9ca0966a11ba5893117
Paul Stroud <paul.stroud2023@gmail.com>
2023-04-25 15:12:32 +0000

    working code

--- 04. file encryption pt1.py
+++ 04. file encryption pt1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 # Script: Ops 401 Class 06 script
 # Author: Paul Stroud
 # Date of latest revision: 04/24/23
@@ -28,37 +27,55 @@
 
 
 
-import sys          # for parsing script args
-# import datetime     # to access and format date/time
-# import time         # for timeout/sleep
-# import re           # for regex
-# import subprocess   # for running bash commands
-
-# import smtplib      # to send emails via SMTP
-# from email.message import EmailMessage  # to craft emails
+import sys          # for breaking out of the script
+import time         # for timeout/sleep
 import os           # to verify if file exists
 
-
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet  # for encryption/decryption
 
 
 ###### FUNCTIONS ########
 
-# save a cryptographic key
-def save_cryptokey(name = "401.06.key"):
+# save cryptographic key to a file
+def save_cryptokey(name):
     keyfile = open(name, "wb")
     keyfile.write(Fernet.generate_key())
 
-def load_cryptokey(name = "401.06.key"):
+# load cryptographic key from a file
+def load_cryptokey(name):
     return open(name, "rb").read()
 
 
-
+# encrypt a file using the given crypto key
 def encrypt_file(name, key):
-    enc_name = name + ".enc"
+    enc_name = name + ".enc"                # encrypted file name
+    cleartext = open(name, "rb").read()     # read the source file
+    ciphertext = Fernet(key).encrypt(cleartext) # encrypt the data
+    open(enc_name, "wb").write(ciphertext)  # write out to a new file
+
+    # prompt the user to delete the original file (default is yes)
+    while True:
+      cln = input(f"Delete the original file {fname}?  (Y/n): ")
+      if cln == "y" or cln == "Y" or cln == "":
+        os.remove(name) # nuke the file
+        break           # break the loop
+      if cln == "n" or cln == "N": break  # break the loop without deleting
     
-def decrypt_file():
-    print("TEST")
+
+# decrypt a file using the given crypto key
+def decrypt_file(name, key):
+    dec_name = name.split(".enc")[0]      # extract decrypted file name
+    ciphertext = open(name, "rb").read()  # read the source file
+    cleartext = Fernet(key).decrypt(ciphertext) # decrypt the data
+    open(dec_name, "wb").write(cleartext) # write out to a new file
+
+    # prompt the user to delete the original file (default is yes)
+    while True:
+      cln = input(f"Delete the original file {fname}?  (Y/n): ")
+      if cln == "y" or cln == "Y" or cln == "":
+        os.remove(name) # nuke the file
+        break           # break the loop
+      if cln == "n" or cln == "N": break  # break the loop without deleting
 
 
 
@@ -76,32 +93,27 @@ def decrypt_msg(msg, key): # `msg` is a string, `key` is the crypto key file
     # decode the plaintext back into a string and return the result
     return Fernet(key).decrypt(msg.encode()).decode('utf-8')
    
-# global dictionary of functions/operations to reference in the main
-op_array = { 1:encrypt_file, 2:decrypt_file, 3:encrypt_msg, 4:decrypt_msg } 
 
+###### MAIN CODE ########
 
-
-
-# menu loop
 key_name = input('Enter the crypto key_name name ("401.06.key" is default): ')
 key_name = "401.06.key" if (key_name == "") else key_name  # if nothing entered, use the default key name
 
-
+# if key file doesn't exist, create a new one
 if not os.path.exists(key_name):
    print(f"Creating a new key {key_name}... ", end="")
    save_cryptokey(key_name)
    print("DONE")
 
+# load the key from the file
 print(f"Loading {key_name}... ", end="")
 key = load_cryptokey(key_name)
 print("DONE")
 
 
-
-
-# print(key)
-# input()
+# menu loop
 while True:
+    time.sleep(1)
     print("\n>>> Encryption Utility v1.0 <<<")
     print("Select the operation to perform:")
     print("\t1. Encrypt a file")
@@ -109,46 +121,45 @@ while True:
     print("\t3. Encrypt a message")
     print("\t4. Decrypt a message")
     print("\t5. Exit")
-    op = 0
+
+    op = 0  # holds user input
     while not (op >= 1 and op <= 5):
       try:
          op = int(input("Enter a menu option (1-5): "))
-      except KeyboardInterrupt: exit
-      except:
+      except KeyboardInterrupt:   # Ctrl+C
+         print()
+         sys.exit()
+      except: # catch all for anything other than 1-5 or Ctrl+C
          print("Invalid input. Please try again.")
 
     if op == 5:
       print("Exiting the script...")
-      sys.exit()
+      sys.exit()  # kill the script
     else:
-      if op == 1:
-         print("1")
-      if op == 2:
-         print("2")
-      if op == 3:
-         plain = input("Enter the message to encrypt: ")
-         cipher = encrypt_msg(plain, key)
-         print(f"Encrypted message: {cipher}")
-      if op == 4:
-         cipher = input("Enter the message to decrypt: ")
-         plain = decrypt_msg(cipher, key)
-         print(f"Decrypted message: {plain}")
+      if op == 1:   # encrypt a file
+        try:
+            fname = input("Enter the file to encrypt: ")
+            encrypt_file(fname, key)
+            print(f"Encrypted file saved as {fname}.enc")
+        except FileNotFoundError: print(f"File {fname} does not exist")
 
-# print("BOOM")
-# input()
+      if op == 2:   # decrypt a file
+        try:
+            fname = input("Enter the file to decrypt: ")
+            decrypt_file(fname, key)
+            print(f"Decrypted file saved as {fname.split('.enc')[0]}")
+        except FileNotFoundError: print(f"File {fname} does not exist")
 
+      if op == 3:   # encrypt a string
+        plain = input("Enter the message to encrypt: ")
+        cipher = encrypt_msg(plain, key)
+        print(f"Encrypted message: {cipher}")
 
+      if op == 4:   # decrypt a file
+        cipher = input("Enter the message to decrypt: ")
+        plain = decrypt_msg(cipher, key)
+        print(f"Decrypted message: {plain}")
 
-# print (Fernet.generate_key())
+print("Script complete!")
 
-
-
-
-
-# test = input("TEST: ")
-# print(test.encode())
-# input()
-
-
-
-
+# ze end
