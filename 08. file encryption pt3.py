e0b963aab8ed1378852a022281e3f22a155d1d9d
Paul Stroud <paul.stroud2023@gmail.com>
2023-04-27 16:30:55 +0000

    buggy but sorta works

--- 08. file encryption pt3.py
+++ 08. file encryption pt3.py
@@ -20,7 +20,124 @@ import os           # to verify if file exists
 from cryptography.fernet import Fernet  # for encryption/decryption
 
 
+
+
+#import winreg
+import tkinter as tk
+import time
+
+
+import ctypes
+import pyautogui
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import datetime
+import time
+
+
+
+
+
+# constants definition
+message = "YOUR DATA WILL BE DESTROYED IN:"
+popup_msg = 'Your data has been ENCRYPTED. \
+             \nYou must pay $1,000,000 to recover it. \
+             \n\n(Press Ctrl+C in the terminal to stop simulation)'
+font_size = 50
+img_path = r'C:\Users\Lab User\NEW_IMG.png'
+
+
+
+
+
 ###### FUNCTIONS ########
+
+# simulated ransomware attack (no actual encryption takes place)
+# adds a popup window and a dynamic wallpaper with a countdown
+def ransom():
+    pyautogui.hotkey('win', 'd') # minimize all windows
+
+    window = tk.Tk()            # make a new window
+    window.geometry("300x150")  # set dimensions
+    window.eval("tk::PlaceWindow . center") # center on the screen
+    
+    # Create label
+    label_var = tk.StringVar()
+    label_var.set(popup_msg)
+    label = tk.Label(window, textvariable=label_var)
+    label.pack()
+
+    # Update and show window once
+    window.update_idletasks()
+    window.update()
+    window.attributes('-topmost', True)
+
+
+
+    
+    start_time = datetime.datetime.now()  # get the current time
+    end_time = start_time + datetime.timedelta(hours=1) # 1 hour deadline
+    time_diff = end_time - start_time # calculate the delta
+
+    width, height = pyautogui.size() # display dimensions (to make new wallpaper)
+
+    loop_count = 0  # counter to use in the loops
+
+    # bomb timer loop
+    try:
+        while time_diff.total_seconds() > 1:
+            
+            start_time = datetime.datetime.now()  # update the current time
+            time_diff = end_time - start_time     # recalculate time delta
+            timer = str(time_diff).split(".")[0]  # extract HH:MM:SS string
+
+            # create a new image with the same size as the display
+            image = Image.new("RGB", (width, height), "orange")
+            draw = ImageDraw.Draw(image)  # create a drawing context
+            font = ImageFont.truetype("arial.ttf", font_size) # font setting for the text
+
+            # place the message in the center, sligthly above
+            text_width, text_height = draw.textsize(message, font=font) # calculate text size
+            x = (width - text_width) / 2    # center horizontally
+            y = (height - text_height) / 2  # center vertically
+            draw.text((x, y-font_size*0.6), message, fill=(0, 0, 0), font=font) # add text to image
+
+            # place the timer in the center, below the message
+            text_width, text_height = draw.textsize(f"{timer}", font=font)
+            x = (width - text_width) / 2
+            y = (height - text_height) / 2
+            draw.text((x, y+font_size*0.6), f"{timer}", fill=(0, 0, 0), font=font)
+
+            # note to self: these two blocks of code could be streamlined via function (stretch goal)
+
+
+            image.save(img_path)  # save the image to a file
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path, 0) # set the new wallpaper
+
+            label_var.set(popup_msg + f'\n{timer}') # change the text in the popup
+            window.update() # update the popup
+
+            loop_count += 1
+            
+            if round(loop_count % 10) == 0:
+                pyautogui.hotkey('win', 'd')  # minimize all windows
+                window.lift()       # raise popup on top of other windows
+                window.deiconify()  # open the popup if it was minimized
+            time.sleep(1)
+
+    except KeyboardInterrupt:   # process Ctrl+C
+        pass    # do nothing; break the loop and continue
+
+    # --- undo the simulation ---
+    # restore default wallpaper
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, r'C:\Windows\Web\Wallpaper\Windows\img0.jpg', 0)
+    window.destroy()  # close the popup
+
+
+
+
 
 # save cryptographic key to a file
 def save_cryptokey(name):
@@ -120,7 +237,7 @@ print("DONE")
 # menu loop
 while True:
     time.sleep(1)
-    print("\n>>> Encryption Utility v2.0 <<<")
+    print("\n>>> Encryption Utility v3.0 <<<")
     print("Select the operation to perform:" \
           "\n\t1. Encrypt a file" \
           "\n\t2. Decrypt a file" \
@@ -128,12 +245,13 @@ while True:
           "\n\t4. Decrypt a message" \
           "\n\t5. Encrypt all files in a folder" \
           "\n\t6. Decrypt all files in a folder" \
+          "\n\t7. Ransomware simulation" \
           "\n\t0. Exit")
 
     op = -1  # holds user input
-    while not (op >= 0 and op <= 6):
+    while not (op >= 0 and op <= 7):
       try:
-         op = int(input("Enter a menu option (0-6): "))
+         op = int(input("Enter a menu option (0-7): "))
       except KeyboardInterrupt:   # Ctrl+C
          print()
          sys.exit()
@@ -189,6 +307,11 @@ while True:
               decrypt_file(i, key, force="y")  # decrypt, force deletion w/o prompt
               print(f"Decrypted {i}")
         else: print(f"Dir {dir_name} does not exist. Please try again.")
+
+      if op == 7:   # ransomware simulation
+        print("Starting ransomware simulation...")
+        time.sleep(1)
+        ransom()
 
 
 print("Script complete!")
