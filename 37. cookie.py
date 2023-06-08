37854cb061c9f8b9c23d29cd7847bd004ba1c94c
Paul Stroud <paul.stroud2023@gmail.com>
2023-06-08 15:32:43 +0000

    done

--- 37. cookie.py
+++ 37. cookie.py
@@ -7,50 +7,45 @@
 # Resources used: google, stackoverflow, github demo, chatgpt
 
 # MAIN REQS:
-# In Python create a script that executes from a Linux box to perform the following:
-#   Prompts the user to type a URL or IP address.
-#   Prompts the user to type a port number.
-#   Performs banner grabbing using netcat against the target address at the target port; prints the results to the screen then moves on to the step below.
-#   Performs banner grabbing using telnet against the target address at the target port; prints the results to the screen then moves on to the step below.
-#   Performs banner grabbing using Nmap against the target address of all well-known ports; prints the results to the screen.
-
-
-
-#!/usr/bin/env python3
-
-# The below Python script shows one possible method to return the cookie from a site that supports cookies.
-
-import requests
-
-# targetsite = input("Enter target site:") # Uncomment this to accept user input target site
-targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
-response = requests.get(targetsite)
-cookie = response.cookies
-
-def bringforthcookiemonster(): # Because why not!
-    print('''
-
-              .---. .---.
-             :     : o   :    me want cookie!
-         _..-:   o :     :-.._    /
-     .-''  '  `---' `---' "   ``-.
-   .'   "   '  "  .    "  . '  "  `.
-  :   '.---.,,.,...,.,.,.,..---.  ' ;
-  `. " `.                     .' " .'
-   `.  '`.                   .' ' .'
-    `.    `-._           _.-' "  .'  .----.
-      `. "    '"--...--"'  . ' .'  .'  o   `.
-
-        ''')
-
-bringforthcookiemonster()
-print("Target site is " + targetsite)
-print(cookie)
-
 # Add here some code to make this script perform the following:
 # - Send the cookie back to the site and receive a HTTP response
 # - Generate a .html file to capture the contents of the HTTP response
 # - Open it with Firefox
-#
-# Stretch Goal
-# - Give Cookie Monster hands
+
+
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+import subprocess
+
+
+cookie_monster_ascii = '\n            _  _' \
+                       '\nme want    (.)(\')' \
+                       '\nmore...   / ___, \  .-.' \
+                       '\n    .-. _ \ \'--\' / (:::) ' \
+                       '\n   (:::{ \'-`--=-`-\' }"`' \
+                       '\n    `-\' `"/      \"`' \
+                       '\n          \      /' \
+                       '\n         _/  /\  \_' \
+                       '\n       {   /  \   }' \
+                       '\n         `"`    `"`'
+
+
+### MAIN ###
+print(cookie_monster_ascii)
+
+targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+print("Target site is " + targetsite)
+print(cookie)
+
+response = requests.get(targetsite, cookies=cookie) # send the cookie back to the site
+
+# put the response into a .html file
+filename = "response.html"
+with open(filename, "w") as file:
+    file.write(response.text)
+
+subprocess.run(["firefox", filename]) # open html with firefox
